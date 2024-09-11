638c26cf277bcf6e43fe74c051521305600e7e3e
IvanVereshchagin <151856964+IvanVereshchagin@users.noreply.github.com>
2024-09-11 12:43:50 +0300

    Update get_klines.py

--- get_klines.py
+++ get_klines.py
@@ -1,19 +1,20 @@
+### ИМПОРТ НЕОБХОДИМЫХ БИБЛИОТЕК ###
+
 from pybit.unified_trading import HTTP
 import pandas as pd
 import getpass
 import datetime
 import numpy as np
 import tqdm
-#Скрипт для получения минутных данных с криптобиржи bybit для бэктестинга и анализа.
 
-# API_KEYS
-#АПИ-КЛЮЧИ К АККАУНТУ
 
-a_k = "..."
-a_sk = "..."
+#АПИ-КЛЮЧИ К ТОРГОВОМУ-АККАУНТУ BYBIT
 
-# DOWNLOADING DATA FROM APRIL 2020 TILL DECEMBER 2023
-#СКАЧИВАЕМ ДАННЫЕ С АПРЕЛЯ 2020 по ДЕКАБРЬ 2023
+a_k = ...
+a_sk = ...
+
+
+#ЗАГРУЗКА ДАННЫХ:  АПРЕЛЬ 2020 - ДЕКАБРЬ 2023
 ts = [ i for i in range(1585699200, 1585699200 + 1440*30*45*60 , 60 * 1000)]
 
 total_quotes = []
@@ -29,8 +30,8 @@ for t in ts :
         api_secret=a_sk
         
         )
-        #WE FETCH EVERY 1000 KLINES (limit) and FULLFILL THE TOTAL DATAFRAME WITH IT.
-        # При апи запросе существует лимит - 1000 свечей/ запрос. 
+        
+        # ПРИ API-ЗАПРОСЕ к BYBIT CУЩЕСТВУЕТ ЛИМИТ В 1000 СВЕЧЕЙ, ПОЭТОМУ КАЖДЫЙ РАЗ ПАРСИМ ПО 1000 СВЕЧЕЙ и ДОБАВЛЯЕМ К ОБЩЕМУ ДАТАФРЕЙМУ
         q = session.get_kline(
         category="linear",
         symbol="BTCUSDT",
@@ -67,5 +68,4 @@ else :
     #Скачиваем итоговый датафрейм
     df = pd.DataFrame(total_quotes)
     df.to_csv('bybit_data.csv' , index = False )
-
-    
+#КАК ТОЛЬКО "ПРОШЛИСЬ" ПО ДИАПАЗОНУ ДАТ И ДОБАВИЛИ ВСЕ СВЕЧИ, СКАЧИВАЕМ ГОТОВЫЙ ДАТАФРЕЙМ    
